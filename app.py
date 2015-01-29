8fce1b008e62b777fdb462aca26fe7f0fa481314
Hsiaoming Yang <me@lepture.com>
2015-01-29 16:32:09 +0800

    Logging for debug

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 # coding: utf-8
 
 import os
+import sys
 import logging
 from zerqu import create_app
 from zerqu.models import db
 
-logger = logging.getLogger('flask_oauthlib')
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+formatter = logging.Formatter(
+    '[%(levelname)s %(funcName)s %(filename)s:%(lineno)d]: %(message)s'
+)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+
+log = logging.getLogger('flask_oauthlib')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+log = logging.getLogger('oauthlib')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
 
 root = os.path.abspath(os.path.dirname(__file__))
 os.environ['ZERQU_CONF'] = os.path.join(root, 'etc/development.py')
