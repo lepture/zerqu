2d0adff0de1db13b957a8123fe3809a30f406d35
Hsiaoming Yang <me@lepture.com>
2015-02-05 18:26:15 +0800

    Fix test cases for users

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,6 +3,7 @@
 import base64
 from flask import json
 from zerqu.models import User
+from ._base import TestCase
 
 
 def b64encode(text):
@@ -14,49 +15,66 @@ auth_header = {
 }
 
 
-def test_create_user_forbidden(client):
-    rv = client.post('/api/user')
-    assert rv.status_code == 403
+class TestCreateUser(TestCase):
+    def test_create_user_forbidden(self):
+        rv = self.client.post('/api/users')
+        assert rv.status_code == 403
+
+    def test_create_user_success(self):
+        rv = self.client.post('/api/users', data=json.dumps({
+            'username': 'createuser',
+            'email': 'createuser@gmail.com',
+            'password': 'test-password',
+        }), content_type='application/json', headers=auth_header)
+        assert rv.status_code == 201
+        data = json.loads(rv.data)
+        user = User.cache.filter_first(username='createuser')
+        expected = {'status': 'ok', 'data': dict(user)}
+        assert data == json.loads(json.dumps(expected))
+
+        # can't register with the same email address
+        rv = self.client.post('/api/users', data=json.dumps({
+            'username': 'createuser2',
+            'email': 'createuser@gmail.com',
+            'password': 'test-password',
+        }), content_type='application/json', headers=auth_header)
+        assert rv.status_code == 400
+        assert b'error_form' in rv.data
+
+    def test_create_user_error_form(self):
+        rv = self.client.post('/api/users', data=json.dumps({
+            'username': 'createuser',
+            'password': 'test-password',
+        }), content_type='application/json', headers=auth_header)
+        assert rv.status_code == 400
+        assert b'error_form' in rv.data
+
+        rv = self.client.post('/api/users', data=json.dumps({
+            'username': 'zerqu',
+            'password': 'test-password',
+        }), content_type='application/json', headers=auth_header)
+        assert rv.status_code == 400
+        assert b'registered' in rv.data
 
 
-def test_create_user_success(client):
-    rv = client.post('/api/user', data=json.dumps({
-        'username': 'createuser',
-        'email': 'createuser@gmail.com',
-        'password': 'test-password',
-    }), content_type='application/json', headers=auth_header)
-    assert rv.status_code == 201
-    data = json.loads(rv.data)
-    user = User.cache.filter_first(username='createuser')
-    expected = {'status': 'ok', 'data': dict(user)}
-    assert data == json.loads(json.dumps(expected))
-
-    # can't register with the same email address
-    rv = client.post('/api/user', data=json.dumps({
-        'username': 'createuser2',
-        'email': 'createuser@gmail.com',
-        'password': 'test-password',
-    }), content_type='application/json', headers=auth_header)
-    assert rv.status_code == 400
-    assert b'error_form' in rv.data
+class TestCurrentUser(TestCase):
+    def test_current_authenticated_user(self):
+        rv = self.client.get('/api/user')
+        assert rv.status_code == 401
 
 
-def test_create_user_error_form(client):
-    rv = client.post('/api/user', data=json.dumps({
-        'username': 'createuser',
-        'password': 'test-password',
-    }), content_type='application/json', headers=auth_header)
-    assert rv.status_code == 400
-    assert b'error_form' in rv.data
+class TestListUsers(TestCase):
+    def test_list_without_parameters(self):
+        rv = self.client.get('/api/users')
+        assert rv.status_code == 200
 
-    rv = client.post('/api/user', data=json.dumps({
-        'username': 'zerqu',
-        'password': 'test-password',
-    }), content_type='application/json', headers=auth_header)
-    assert rv.status_code == 400
-    assert b'registered' in rv.data
+    def test_list_with_before(self):
+        rv = self.client.get('/api/users?before=2')
+        value = json.loads(rv.data)
+        assert len(value['data']) == 1
 
-
-def test_current_authenticated_user(client):
-    rv = client.get('/api/user')
-    assert rv.status_code == 401
+    def test_list_with_after(self):
+        rv = self.client.get('/api/users?after=1')
+        value = json.loads(rv.data)
+        for item in value['data']:
+            assert item['id'] != 1
