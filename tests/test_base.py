319c9a7ea04b30859df65d6e6f893e56e3461a69
Hsiaoming Yang <me@lepture.com>
2015-02-06 12:34:30 +0800

    Fix test cases. Add test cases for model events

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from zerqu.versions import API_VERSION
+from zerqu.models import db, User
 from ._base import TestCase
 
 
@@ -8,3 +9,48 @@ class TestAPI(TestCase):
     def test_get_api_index(self):
         rv = self.client.get('/api/')
         assert API_VERSION in rv.data
+
+
+class TestModel(TestCase):
+    def test_model_events(self):
+        user = User(username='hello', email='hello@gmail.com')
+        db.session.add(user)
+        db.session.commit()
+
+        # get from database
+        assert user == User.cache.get(user.id)
+        # get from cache
+        cached_user = User.cache.get(user.id)
+        assert user != cached_user
+        assert user.id == cached_user.id
+
+        # update cache
+        user.username = 'jinja'
+        db.session.add(user)
+        db.session.commit()
+        assert User.cache.get(user.id).username == 'jinja'
+
+        # delete cache
+        db.session.delete(user)
+        db.session.commit()
+        assert User.cache.get(user.id) is None
+
+    def test_get_many_dict(self):
+        assert User.cache.get_dict([]) == {}
+
+        for i in range(10):
+            user = User(username='foo-%d' % i, email='foo-%d@gmail.com' % i)
+            db.session.add(user)
+        db.session.commit()
+
+        first_id = User.cache.filter_first(username='foo-0').id
+        idents = [first_id + i for i in range(10)]
+        missed = User.cache.get_dict(idents)
+        assert len(missed.keys()) == 10
+
+        cached = User.cache.get_dict(idents)
+        assert missed.keys().sort() == cached.keys().sort()
+
+        missed_names = [o.username for o in missed.values()]
+        cached_names = [o.username for o in User.cache.get_many(idents)]
+        assert missed_names.sort() == cached_names.sort()
--- tests/test_libs.py
+++ tests/test_libs.py
@@ -2,46 +2,48 @@
 
 from zerqu.libs.ratelimit import ratelimit
 from zerqu.libs.utils import is_robot, is_mobile
+from ._base import TestCase
 
 
-def test_ratelimit(app):
-    remaining, expires = ratelimit('test:ratelimit', 20, 20)
-    assert remaining == 19
-    assert expires == 20
-
-    for i in range(20):
+class TestRateLimit(TestCase):
+    def test_ratelimit(self):
         remaining, expires = ratelimit('test:ratelimit', 20, 20)
-    assert remaining == 0
+        assert remaining == 19
+        assert expires == 20
+
+        for i in range(20):
+            remaining, expires = ratelimit('test:ratelimit', 20, 20)
+        assert remaining == 0
 
 
-def test_is_robot(app):
+class TestUserAgent(TestCase):
+    def test_is_robot(self):
+        app = self.app
 
-    @app.route('/test-is-robot')
-    def is_robot_view():
-        if is_robot():
-            return 'yes'
-        return 'no'
+        @app.route('/test-is-robot')
+        def is_robot_view():
+            if is_robot():
+                return 'yes'
+            return 'no'
 
-    client = app.test_client()
-    rv = client.get('/test-is-robot', headers={
-        'User-Agent': 'Googlebot-Image/1.0'
-    })
-    assert b'yes' == rv.data
+        rv = self.client.get('/test-is-robot', headers={
+            'User-Agent': 'Googlebot-Image/1.0'
+        })
+        assert b'yes' == rv.data
 
+    def test_is_mobile(self):
+        ua = (
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) '
+            'AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 '
+            'Mobile/10A5376e Safari/8536.25'
+        )
+        app = self.app
 
-def test_is_mobile(app):
-    iphone6 = (
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) '
-        'AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e '
-        'Safari/8536.25'
-    )
+        @app.route('/test-is-mobile')
+        def is_mobile_view():
+            if is_mobile():
+                return 'yes'
+            return 'no'
 
-    @app.route('/test-is-mobile')
-    def is_mobile_view():
-        if is_mobile():
-            return 'yes'
-        return 'no'
-
-    client = app.test_client()
-    rv = client.get('/test-is-mobile', headers={'User-Agent': iphone6})
-    assert b'yes' == rv.data
+        rv = self.client.get('/test-is-mobile', headers={'User-Agent': ua})
+        assert b'yes' == rv.data
