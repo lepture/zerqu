93ed342b1799c03f44a806f7204056e7418a7c9c
Hsiaoming Yang <me@lepture.com>
2015-02-28 20:19:29 +0800

    Define scopes

--- zerqu/scopes.py
+++ zerqu/scopes.py
@@ -6,7 +6,36 @@
     Define available scopes for OAuth.
 """
 
-PUBLIC_SCOPES = [
+#: define available scopes
+SCOPES = [
+    ('user:email', 'read your email address'),
+    ('user:write', 'change your account information'),
+    ('user:follow', 'follow and unfollow other users'),
+    ('user:subscribe', 'follow and unfollow a cafe'),
+
+    ('cafe:private', 'read your private cafes'),
+    ('cafe:write', 'create and update your cafes'),
+
+    ('topic:write', 'create topics with your account'),
+    ('topic:delete', 'delete your topics'),
+    ('comment:write', 'create a comment with your account'),
+    ('comment:delete', 'delete your comments'),
+]
+
+#: alias scopes
+ALIASES = {
+    'user': ['user:email', 'user:write', 'user:follow', 'user:subscribe'],
+    'cafe': ['cafe:write', 'cafe:private'],
+    'topic': ['topic:write', 'topic:delete'],
+    'comment': ['comment:write', 'comment:delete'],
+}
+
+#: scopes that user can choose to disable
+CHOICES = [
+    'user:email',
+    'cafe:private',
+    'topic:delete',
+    'comment:delete',
 ]
 
 #: scopes that user can choose to grant
@@ -16,10 +45,6 @@ USER_SCOPES = [
     ('user:topic', 'create a topic under your name'),
     ('user:follow', 'follow and unfollow other users'),
     ('user:subscribe', 'follow and unfollow a cafe'),
-]
-
-#: scopes that only available for confidential clients
-CONFIDENTIAL_SCOPES = [
 ]
 
 
