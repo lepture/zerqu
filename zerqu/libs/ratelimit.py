7d0fcc6a2143faff7b5d56bce5fc1523cefe418e
Hsiaoming Yang <me@lepture.com>
2015-06-30 20:19:21 +0800

    Prepare a redis RateLimiter to replace cache limiter

--- zerqu/libs/ratelimit.py
+++ zerqu/libs/ratelimit.py
@@ -8,27 +8,54 @@ from .cache import cache
 logger = logging.getLogger('zerqu')
 
 
-def ratelimit(prefix, count=600, duration=300):
-    logger.info('Rate limit on %s' % prefix)
-    count_key = '%s$c' % prefix
-    reset_key = '%s$r' % prefix
-    remaining, resetting = cache.get_many(count_key, reset_key)
+class Ratelimiter(object):
+    def __init__(self, db):
+        self.db = db
 
-    if not remaining and not resetting:
-        remaining = count - 1
-        expires_at = duration + int(time.time())
-        cache.set_many({
-            count_key: remaining,
-            reset_key: expires_at,
+    def get_data(self):
+        return self.db.get_many(self.count_key, self.reset_key)
+
+    def create(self, remaining, expires_at, duration):
+        self.db.set_many({
+            self.count_key: remaining,
+            self.reset_key: expires_at,
         }, duration)
-        expires = duration
-    else:
-        if resetting:
-            expires = int(resetting) - int(time.time())
+
+    def remain(self, remaining, expires):
+        self.db.set(self.count_key, remaining, expires)
+
+    def __call__(self, prefix, count=600, duration=300):
+        logger.info('Rate limit on %s' % prefix)
+        self.count_key = '%s$c' % prefix
+        self.reset_key = '%s$r' % prefix
+
+        remaining, resetting = self.get_data()
+        if not remaining and not resetting:
+            remaining = count - 1
+            expires_at = duration + int(time.time())
+            self.create(remaining, expires_at, duration)
+            expires = duration
         else:
-            expires = 0
-        if remaining <= 0 and expires:
-            return remaining, expires
-        remaining = int(remaining) - 1
-        cache.set(count_key, remaining, expires)
-    return remaining, expires
+            expires = int(resetting) - int(time.time())
+            if remaining <= 0 and expires:
+                return remaining, expires
+            remaining = int(remaining) - 1
+            self.remain(remaining, expires)
+        return remaining, expires
+
+
+class RedisRateLimiter(Ratelimiter):
+    def get_data(self):
+        return self.db.mget(self.count_key, self.reset_key)
+
+    def create(self, remaining, expires_at, duration):
+        with self.db.pipeline() as pipe:
+            pipe.set(self.count_key, remaining, ex=duration, nx=True)
+            pipe.set(self.reset_key, expires_at, ex=duration, nx=True)
+            pipe.execute()
+
+    def remain(self, remaining, expires):
+        self.db.set(self.count_key, remaining, ex=expires, xx=True)
+
+
+ratelimit = Ratelimiter(cache)
