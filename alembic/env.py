602573347636229fd50822a50f1bdfdb3c9c61c8
Hsiaoming Yang <me@lepture.com>
2015-07-18 22:52:49 +0800

    Prepare alembic for version control

---
+++ alembic/env.py
@@ -0,0 +1,80 @@
+from __future__ import with_statement
+
+import os
+import sys
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from logging.config import fileConfig
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add zerqu root to sys.path
+cwd = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, cwd)
+
+from zerqu.app import create_app
+from zerqu.models import db
+
+if 'ZERQU_CONF' not in os.environ:
+    os.environ['ZERQU_CONF'] = os.path.join(cwd, 'etc/secret.py')
+
+app = create_app()
+db.init_app(app)
+
+config.set_main_option(
+    'sqlalchemy.url', app.config.get('SQLALCHEMY_DATABASE_URI')
+)
+target_metadata = db.metadata
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
