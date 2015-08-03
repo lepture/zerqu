f1da41e21d1750f86b0a3f43aaacac1f19debe54
Hsiaoming Yang <me@lepture.com>
2015-08-03 22:41:05 +0800

    API home contains more data

--- zerqu/api/front.py
+++ zerqu/api/front.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from flask import jsonify
+from flask import current_app, request
 from .base import ApiBlueprint
 from ..versions import VERSION, API_VERSION
 
@@ -9,4 +10,12 @@ api = ApiBlueprint('')
 
 @api.route('')
 def index():
-    return jsonify(system='zerqu', version=VERSION, api_version=API_VERSION)
+    config = current_app.config
+    return jsonify(
+        system='zerqu',
+        version=VERSION,
+        api_version=API_VERSION,
+        site=config.get('SITE_NAME'),
+        description=config.get('SITE_DESCRIPTION'),
+        remote_addr=request.remote_addr,
+    )
