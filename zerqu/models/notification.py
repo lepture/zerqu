adc5df44fbf9d10cab6cc63d4e888bce3254f24c
Hsiaoming Yang <me@lepture.com>
2015-08-13 23:44:14 +0800

    Design notification with redis

--- zerqu/models/notification.py
+++ zerqu/models/notification.py
@@ -1,29 +1,43 @@
 # coding: utf-8
 
 import datetime
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import SmallInteger, Integer
-
-from .base import Base, JSON
+from flask import json
+from zerqu.libs.cache import redis
+from zerqu.libs.utils import Pagination
 
 
-class Notification(Base):
-    __tablename__ = 'zq_notification'
+class Notification(object):
+    CATEGORY_COMMENT = 'comment'
+    CATEGORY_MENTION = 'mention'
+    CATEGORY_LIKE_TOPIC = 'like_topic'
+    CATEGORY_LIKE_COMMENT = 'like_comment'
 
-    STATUS_NEW = 1
-    STATUS_READ = 0
+    def __init__(self, user_id):
+        self.user_id
+        self.key = 'notification_list:{}'.format(user_id)
 
-    CATEGORY_COMMENT = 1
-    CATEGORY_MENTION = 2
+    def add(self, sender_id, category, topic_id, **kwargs):
+        kwargs['sender_id'] = sender_id
+        kwargs['topic_id'] = topic_id
+        kwargs['category'] = category
+        kwargs['created_at'] = datetime.datetime.utcnow()
+        redis.lpush(self.key, json.dumps(kwargs))
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False, index=True)
-    sender_id = Column(Integer)
+    def count(self):
+        return redis.llen(self.key)
 
-    status = Column(SmallInteger, default=STATUS_NEW, index=True)
-    category = Column(SmallInteger)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    info = Column(JSON)
+    def get(self, index):
+        rv = redis.lrange(self.key, index, index)
+        if rv:
+            return rv[0]
+        return None
 
-    __reference__ = {'sender': 'sender_id'}
+    def flush(self):
+        redis.delete(self.key)
+
+    def paginate(self, page=1, perpage=20):
+        total = self.count()
+        p = Pagination(total, page=page, perpage=perpage)
+        start = (p.page - 1) * p.perpage
+        stop = start + p.perpage
+        return redis.lrange(self.key, start, stop), p
