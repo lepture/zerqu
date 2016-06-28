131c962e7e56f8b2e078e39698a09632da87efb6
Hsiaoming Yang <me@lepture.com>
2016-06-28 23:01:02 +0900

    Use docker for development

--- local_config.py
+++ local_config.py
@@ -2,25 +2,9 @@ import os
 
 
 SECRET_KEY = 'secret'
-
-if os.path.isfile('/.dockerenv'):
-    is_docker = True
-else:
-    is_docker = False
-
-if is_docker:
-    DB_CONN = 'postgres:secret@db'
-else:
-    DB_CONN = 'postgres@localhost'
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://{conn}/development'.format(conn=DB_CONN)
 SQLALCHEMY_NATIVE_UNICODE = False
-
-if is_docker:
-    REDIS_HOST = 'redis'
-else:
-    REDIS_HOST = 'localhost'
-ZERQU_REDIS_URI = 'redis://{}:6379/0'.format(REDIS_HOST)
-ZERQU_CACHE_REDIS_HOST = REDIS_HOST
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:secret@postgres/development'
+ZERQU_REDIS_URI = 'redis://redis:6379/0'
+ZERQU_CACHE_REDIS_HOST = 'redis'
 
 DEBUG = True
